4c3b28abca143a4650fcb3e9f834a788b37e99d6
Amisha PRIYA <priyaamisha03@gmail.com>
2024-05-22 21:48:57 +0530

    Managing user inputs via state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+
 export default function Login() {
+  /**
+   * Manage user inputs via state
+   */
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    phone: ''
+  });
+
+  function handleInputChange(value, identifier) {
+    setEnteredValues((enteredValues) => ({
+      ...enteredValues,
+      [identifier]: value
+    }));
+  }
+
   function handleSubmit(event) {
     event.preventDefault(); // Prevents the automatic trigger of HTML request to transfer the form data to the server (Happens when onClick prop is used on button)
     console.log("Submitted");
+    console.log(enteredValues);
   }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -10,12 +29,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input 
+            id="email" 
+            type="email" 
+            name="email" 
+            onChange={(event) => handleInputChange(event.target.value, 'email')}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password" 
+            onChange={(event) => handleInputChange(event.target.value, 'phone')}
+            value={enteredValues.phone}
+          />
         </div>
       </div>
 
